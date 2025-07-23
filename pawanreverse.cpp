939000dfa04be8ab2926f6e79b4a5fc853d24751
pawanvermaf <pavanverma70245@gmail.com>
2025-07-23 17:08:26 +0530

    xyz

---
+++ pawanreverse.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    
+    cout << "Enter a string: ";
+    cin >> str;
+
+    int start = 0;
+    int end = str.length() - 1;
+
+    
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+
+        start++;
+        end--;
+    }
+
+    cout << "Reversed string: " << str << endl;
+
+    return 0;
+}
+
+}
