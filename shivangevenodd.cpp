819bbe843ee1e5af112ef42a5c35f46b6920d981
nagarshivang <nagarshivang129@gmail.com>
2025-07-23 05:00:43 -0700

    even odd

---
+++ shivangevenodd.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+using  namespace std;
+int main(){
+    int num=3;
+    if(num%2==0){
+        cout<<"even number";
+    }
+    else{
+        cout<<" number is odd";
+    }
+}
