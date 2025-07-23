96542ad7945819135e947a2c269f3d47ef42cba9
Jatin-11dev <jattfact@gmail.com>
2025-07-23 17:08:49 +0530

    change

---
+++ jatinreverse.cpp
@@ -0,0 +1,26 @@
+ 
+ 
+ 
+ 
+ 
+ #include <iostream>
+using namespace std;
+
+int main() {
+    string str;
+    
+    cout << "Enter a string: ";
+    cin >> str;  
+
+   
+    int length = str.length();
+
+   
+    for (int i = length - 1; i >= 0; i--) {
+        cout << str[i];
+    }
+
+    cout << endl;
+
+    return 0;
+}
